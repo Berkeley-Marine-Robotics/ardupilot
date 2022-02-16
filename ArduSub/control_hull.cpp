f7c924f7ff8e4be14908a7e9e05314105a3fdc5a
AlexImmas <alexandre.immas@gmail.com>
2022-02-16 11:23:03 -0800

    New hull mode 2

---
+++ ArduSub/control_hull.cpp
@@ -0,0 +1,22 @@
+// BMR Hull following flight mode
+// DVL required - Deadk reckoning
+#include "Sub.h"
+
+// hull_init - initialize hull following controller
+bool Sub::hull_init()
+{
+
+	// TBD
+
+	return true;
+}
+
+
+// hull run - runs hull following controller
+void Sub::hull_run()
+{
+	// TBD
+
+	// Add log here
+
+}
